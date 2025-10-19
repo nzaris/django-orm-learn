07cad95559d58b09fe1ec22218981d730a2307cc
nzaris <161066088+nzaris@users.noreply.github.com>
2025-10-19 12:54:30 +0200

    Create Task model using Django ORM

--- lesson/models.py
+++ lesson/models.py
@@ -4,9 +4,25 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    role = models.CharField(max_length=15, choices=[("admin","user")])
+    role = models.CharField(max_length=15, choices=[("admin","Admin"),("user","User")]
 
     def __str__(self):
         return self.name
     
 
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=50 , choices=[
+        ("in progress", "In progress"),
+        ("completed", "Completed")
+        ("postponed", "Postpomed")
+    ], default="in_progress",
+                              
+    )
+
+    assigned_to = models.ForeignKey(User, on_delete=models,SET_NULL, null=True, blank=True)
+
+
+def __str__(self):
+    return f"{self.title} - {self.status}"
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import django_setup
-from lesson.models import User
+from lesson.models import User , Task 
 
 
 
@@ -14,4 +14,25 @@ user2.role = "admin"
 user2.save()
 
 
-print(User.objects.get(id=2))
+print(User.objects.get(id=2).role)
+
+task1 = Task.objects.create(
+title="Зробити систему jackів",
+description="Зробити систему управління jackами",
+assigned_to=User.objects.get(id=2),
+)
+task2 = Task.objects.create(
+title="Створити Django моделі для системи блогу та коментарів",
+description="Створіть екземпляри постів та коментарів та забезпечте взаємодію, наприклад,
+додавання коментарів до посту чи редагування посту.");
+assigned_to=User.objects.get(id=3),
+)
+print(Task.objects.all())
+
+task1.status = "completed"
+task1.save()
+print(task1)
+
+task2.assigned_to = User.objects.get(id=1)
+task2.save()
+print(task2, task2.assigned_to)
